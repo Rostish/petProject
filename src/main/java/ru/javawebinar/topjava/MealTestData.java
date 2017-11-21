13d1d1426e4a68ef41d159906a9a55975f24a491
Rostish <santylus@mail.ru>
2017-11-21 14:31:39 +1000

    3 13 fix servlet

--- src/main/java/ru/javawebinar/topjava/MealTestData.java
+++ src/main/java/ru/javawebinar/topjava/MealTestData.java
@@ -7,17 +7,26 @@ import ru.javawebinar.topjava.model.User;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MealTestData {
     public static final int MEAL_ID = 100002;
 
-    public static final Meal MEAL =  new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+    public static final Meal MEAL =  new Meal(100002,LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Dinner", 500);
+    public static final List<Meal> MEALS = Arrays.asList(
+            new Meal(100002,LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Dinner", 500),
+            new Meal(100003,LocalDateTime.of(2015, Month.MAY, 31, 11, 0), "Dinner", 500),
+            new Meal(100004,LocalDateTime.of(2015, Month.MAY, 31, 12, 0), "Dinner", 500),
+            new Meal(100005,LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Dinner", 500),
+            new Meal(100006,LocalDateTime.of(2015, Month.MAY, 31, 11, 0), "Dinner", 500),
+            new Meal(100007,LocalDateTime.of(2015, Month.MAY, 31, 12, 0), "Dinner", 500)
 
+    );
 
     public static void assertMatch(Meal actual, Meal expected) {
-        assertThat(actual).isEqualToIgnoringGivenFields(expected, "registered", "roles");
+        assertThat(actual).isEqualTo(expected);
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -13,10 +13,13 @@ import ru.javawebinar.topjava.model.User;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
-import static ru.javawebinar.topjava.MealTestData.MEAL_ID;
-import static ru.javawebinar.topjava.MealTestData.assertMatch;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -36,10 +39,14 @@ public class MealServiceTest {
 
     @Test
     public void get() throws Exception {
+        Meal meal = mealService.get(MEAL_ID,USER_ID);
+        assertMatch(meal, MEAL);
     }
 
     @Test
     public void delete() throws Exception {
+        mealService.delete(MEAL_ID,USER_ID);
+        //assertMatch(mealService.getAll(), );
     }
 
     @Test
@@ -52,8 +59,9 @@ public class MealServiceTest {
 
     @Test
     public void getAll() throws Exception {
+        List<Meal> all = mealService.getAll(USER_ID);
+        assertThat(all).isEqualTo(Arrays.asList(MEALS));
     }
-
     @Test
     public void update() throws Exception {
     }
