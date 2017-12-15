7d8b870db8495b60bae8288502540a2c6f4fa942
Rostish <santylus@mail.ru>
2017-12-15 20:30:17 +1000

    move methods from servlet into controller

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -3,17 +3,25 @@ package ru.javawebinar.topjava.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Controller
 public class RootController {
     @Autowired
-    private UserService service;
+    private UserService uService;
+
+    @Autowired
+    private MealService mService;
 
     @GetMapping("/")
     public String root() {
@@ -22,7 +30,7 @@ public class RootController {
 
     @GetMapping("/users")
     public String users(Model model) {
-        model.addAttribute("users", service.getAll());
+        model.addAttribute("users", uService.getAll());
         return "users";
     }
 
@@ -30,6 +38,52 @@ public class RootController {
     public String setUser(HttpServletRequest request) {
         int userId = Integer.valueOf(request.getParameter("userId"));
         AuthorizedUser.setId(userId);
-        return "redirect:meals";
+        return "redirect:users";
     }
+
+    @GetMapping("/meals")
+    public String meals(Model model) {
+        int userId = AuthorizedUser.id();
+        model.addAttribute("meals", MealsUtil.getWithExceeded(mService.getAll(userId), AuthorizedUser.getCaloriesPerDay()));
+        return "meals";
+    }
+
+    @GetMapping("/meals/delete&{id}")
+    public String delete(@PathVariable("id") int id) {
+        int userId = AuthorizedUser.id();
+        mService.delete(id, userId);
+        return "redirect:/meals";
+    }
+
+    @GetMapping("/meals/update")
+    public String update(@RequestParam("id") int id, HttpServletRequest request) {
+        int userId = AuthorizedUser.id();
+        Meal meal = mService.get(id, userId);
+        request.setAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @GetMapping("/meals/create")
+    public String create(HttpServletRequest request) {
+        final Meal meal =
+                new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        request.setAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @PostMapping("/meals/change")
+    public String change(HttpServletRequest request) {
+        int userId = AuthorizedUser.id();
+        int id = Integer.parseInt(request.getParameter("id"));
+        Meal meal = new Meal(id,LocalDateTime.parse(request.getParameter("dateTime"))
+                , request.getParameter("description"), Integer.parseInt(request.getParameter("calories")));
+        if (request.getParameter("id").isEmpty()) {
+            mService.create(meal, userId);
+        } else {
+            mService.update(meal, userId);
+        }
+        return "redirect:/meals";
+    }
+
+
 }
